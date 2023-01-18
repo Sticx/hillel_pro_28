d1a8710275eb6bd9bee0c867b907590c4d0b91a6
Sticx <108312079+Sticx@users.noreply.github.com>
2023-01-18 16:37:55 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
-function App() {
+export function App() {
+  const [todos, setTodo] = useState([]);
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    const todo = {
+      id: Math.random() * 10,
+      text: event.target[0].value,
+      isDone: false
+    }
+    setTodo([...todos, todo]);
+    event.target[0].value = '';
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div className={"todo-container"}>
+        <h1>To-Do List</h1>
+        <form action="#" onSubmit={addTodo}>
+          <input type="text"/>
+          <button>Add</button>
+        </form>
+        <h2>List of tasks:</h2>
+        <div className="todos-block">
+          {todos.map((todo, index) => {
+            return (
+                <div style={{margin: "20px"}} key={index}>
+                  <label>
+                    <input type="checkbox"/>
+                    <span>{todo.text}</span>
+                    <button>Delete</button>
+                  </label>
+                </div>
+            )
+          })}
+        </div>
+      </div>
+  )
 
-export default App;
+}
